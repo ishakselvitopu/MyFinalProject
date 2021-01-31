9468a7099074d8dc09f97f4fb599b33d32706d06
ishak selvitopu <selvitopuishak@gmail.com>
2021-01-31 17:22:04 +0300

    Add project files.

---
+++ Entities/Abstract/IEntity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Abstract
+{
+    /*
+     * IEntity implement eden class bir veritabanı tablosudur.
+     * Concrete klasöründe bulunan her sınıf  IEntity ile implemente olucak.
+     */
+   public interface IEntity
+    {
+    }
+}
